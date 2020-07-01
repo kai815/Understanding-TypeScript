3881490fdd6f2c92e55839960bccabecee269ef4
kai815 <h.kaizuka815@gmail.com>
2020-07-01 09:19:49 +0900

    enum

--- app.ts
+++ app.ts
@@ -2,18 +2,18 @@
 //   name: string
 //   age: number
 // } 
-const person:{
-  name: string
-  age:number
-  hobbies: string[]
-  // Tuple
-  role: [number, string]
-} = {
-  name: 'yota',
-  age:30,
-  hobbies: ['Sports', 'Cooking'],
-  role:[2, 'auther']
-}
+// const person:{
+//   name: string
+//   age:number
+//   hobbies: string[]
+//   // Tuple
+//   role: [number, string]
+// } = {
+//   name: 'yota',
+//   age:30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role:[2, 'auther']
+// }
 
 
 // person.role.push('admin')
@@ -23,6 +23,21 @@ const person:{
 // person.role[1] = 10
 // person.role = [0, 'admin', 'user']
 
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTEHR = 2
+
+enum Role {
+  ADMIN, READ_ONLY, AUTHOR
+}
+const person = {
+  name: 'yota',
+  age:30,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN
+}
+
+
 let favoriteActivities: string[]
 favoriteActivities = ['Sports']
 console.log(person.name)
@@ -30,4 +45,8 @@ console.log(person.name)
 for (const hobby of person.hobbies){
   console.log(hobby.toUpperCase())
   // console.log(hobby.map)
+}
+
+if(person.role === Role.ADMIN){
+  console.log("管理者ユーザ")
 }
