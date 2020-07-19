c186043306144c3a604aef1a9f8dbd6e93f9013e
kai815 <h.kaizuka815@gmail.com>
2020-07-20 06:58:03 +0900

    private public

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employess: string[] = []
 
   constructor(n: string) {
     this.name = n
@@ -9,15 +10,32 @@ class Department {
   describe(this: Department){
     console.log('Department: ' + this.name)
   }
+  addEmployee(employee: string) {
+    // validation etc
+    this.employess.push(employee)
+  }
+
+  printEmployeeInformation(){
+    console.log(this.employess.length)
+    console.log(this.employess)
+  }
 }
 
 const accounting = new Department("Accounting")
-console.log(accounting)
+
+accounting.addEmployee('Max')
+accounting.addEmployee('Tom')
+
+//下記のように追加されてしまうのは問題
+// (例：addEmployeeにvalidation記述してたらスルーされる)
+// accounting.employess[2] = 'Anna'
+
 accounting.describe()
+accounting.printEmployeeInformation()
 
-const accountingCopy = { 
-  name: "COPY",
-  describe: accounting.describe 
-}
+// const accountingCopy = { 
+//   name: "COPY",
+//   describe: accounting.describe 
+// }
 
-accountingCopy.describe()
+// accountingCopy.describe()
