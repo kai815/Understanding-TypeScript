114fe368614730c519f98c5ea0c4bd3c3c11774d
kai815 <h.kaizuka815@gmail.com>
2020-07-31 21:58:29 +0900

    class interface

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,37 @@
-interface Person {
+interface Greatable {
   name: string
-  age: number
+  // age: number
 
   greet(phrase: string) : void
 }
+//インターフェイスはオブジェクト型のみに使える
+//複数のクラスに使用可能
 
-let user1: Person
+// 下記のようにもかける
+// type Person = {
+//   name: string
+//   age: number
 
-user1 = {
-  name: 'Max',
-  age: 30,
+//   greet(phrase: string) : void
+// }
+
+// クラスにインターフェイスは複数使える
+class Person implements Greatable {
+  name: string
+  age = 30
+
+  constructor(n: string){
+    this.name = n
+  }
+
   greet(phrase: string){
     console.log(phrase + ' ' + this.name)
   }
 }
 
+let user1: Greatable
+
+user1 = new Person('Max')
+
 user1.greet("ハロー")
+console.log(user1)
