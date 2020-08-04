19dab5a6ef88b6d0237a92f49921ddd07eae4ae7
kai815 <h.kaizuka815@gmail.com>
2020-08-05 08:53:52 +0900

    type gard

--- src/app.ts
+++ src/app.ts
@@ -24,3 +24,60 @@ type Universal = Combinable & Numeric
 
 //number型
 const n1: Universal = 2
+
+function add(a: Combinable, b: Combinable){
+  //型ガード
+  if(typeof a === 'string' || typeof b === 'string'){
+    return a.toString() + b.toString()
+  }
+  return a + b
+}
+
+type UnknownEmployee = Employee | Admin
+
+function printEmployeeInfomation(emp: UnknownEmployee){
+  console.log(emp.name)
+  //in演算子で型ガード
+  if('privileges' in emp){
+    console.log("Privileges: "+ emp.privileges)
+  }
+
+  if('startDate' in emp){
+    console.log("startDate: "+ emp.startDate)
+  }
+}
+printEmployeeInfomation(e1)
+printEmployeeInfomation({name: 'Manu', startDate: new Date()})
+
+class Car {
+  drive(){
+    console.log("運転中・・・・")
+  }
+}
+
+class Truck {
+  drive(){
+    console.log("トラックを運転中・・・・")
+  }
+  loadCargo(amount: number){
+    console.log(`荷物を載せています.....${amount}`)
+  }
+}
+
+type Vehicle = Car | Truck
+
+const v1 = new Car()
+const v2 = new Truck()
+
+function useVehicle(vehicle: Vehicle){
+  vehicle.drive()
+  // if('loadCargo' in vehicle){
+  //   vehicle.loadCargo(1000)
+  // }
+  // インスタンスオブで型ガード
+  if(vehicle instanceof Truck){
+    vehicle.loadCargo(1000)
+  }
+}
+useVehicle(v1)
+useVehicle(v2)
