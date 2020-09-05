2f7a5fa81492a1ef900a01a44c9319f788e95a87
kai815 <h.kaizuka815@gmail.com>
2020-09-05 17:04:28 +0900

    install and setup typescript-eslint

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,26 @@
 class ProjectInput {
-  templateElement: HTMLTemplateElement
-  hostElement: HTMLDivElement
-  formElement: HTMLFormElement
-  constructor(){
-    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
-    this.hostElement = document.getElementById('app')! as HTMLDivElement
-    const importedNode = document.importNode(this.templateElement.content, true)
-    this.formElement = importedNode.firstElementChild as HTMLFormElement
-    this.attach()
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  formElement: HTMLFormElement;
+  constructor() {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
   }
 
   private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.formElement)
+    this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
   }
 }
 
-const projectInput = new ProjectInput()
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const projectInput = new ProjectInput();
