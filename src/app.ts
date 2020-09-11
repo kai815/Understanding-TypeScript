3c0cacb1ec836e5acf92b17c6617e5ae96d0f73f
kai815 <h.kaizuka815@gmail.com>
2020-09-11 09:36:23 +0900

    add projects filtering and modify renderProjects method

--- src/app.ts
+++ src/app.ts
@@ -141,7 +141,13 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
     this.attach();
@@ -149,7 +155,9 @@ class ProjectList {
   }
 
   private renderProjects() {
-    const listEl = document.getElementById(`${this.type}-projects-list`);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const listEl = document.getElementById(`${this.type}-projects-list`)!;
+    listEl.innerHTML = "";
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement("li");
       listItem.textContent = prjItem.title;
