9a7790c30c0f48e4e8a20ac7c93c0809964153d7
kai815 <h.kaizuka815@gmail.com>
2020-09-23 07:03:26 +0900

    variety of import and export

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,9 +1,9 @@
-import { Component } from "../components/base-component.js";
-import { Validatable, validate } from "../util/validation.js";
+import Cmp from "../components/base-component.js";
+import * as Validation from "../util/validation.js";
 import { Autobind } from "../decorators/autobind.js";
 import { projectState } from "../state/project-state.js";
 // ProjectInput class
-export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+export class ProjectInput extends Cmp<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLTextAreaElement;
   mandayInputElement: HTMLInputElement;
@@ -32,28 +32,28 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredManday = this.mandayInputElement.value;
 
-    const titleValidatable: Validatable = {
+    const titleValidatable: Validation.Validatable = {
       value: enteredTitle,
       required: true,
     };
-    const descriptionValidatable: Validatable = {
+    const descriptionValidatable: Validation.Validatable = {
       value: enteredDescription,
       required: true,
       minLength: 5,
     };
-    const mandayValidatable: Validatable = {
+    const mandayValidatable: Validation.Validatable = {
       value: +enteredManday,
       required: true,
       min: 1,
       max: 1000,
     };
-    console.log(validate(titleValidatable));
-    console.log(validate(descriptionValidatable));
-    console.log(validate(mandayValidatable));
+    console.log(Validation.validate(titleValidatable));
+    console.log(Validation.validate(descriptionValidatable));
+    console.log(Validation.validate(mandayValidatable));
     if (
-      !validate(titleValidatable) ||
-      !validate(descriptionValidatable) ||
-      !validate(mandayValidatable)
+      !Validation.validate(titleValidatable) ||
+      !Validation.validate(descriptionValidatable) ||
+      !Validation.validate(mandayValidatable)
     ) {
       alert("入力が正しくありません。再度お試しください。");
       return;
