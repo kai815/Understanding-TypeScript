90a84b167ee8b9dfbd0ac336211c8fea6c4067ed
kai815 <h.kaizuka815@gmail.com>
2020-09-28 06:54:22 +0900

    use class-transformer

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
 //ブラウザでimportをする時は.js必要 webpackなどを使う時は拡張子不要
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
@@ -9,9 +11,10 @@ const products = [
   {title:'商品1', price:100},
   {title:'商品2', price:200}
 ]
-const loadedProducts = products.map(product =>{
-  return new Product(product.title, product.price)
-})
+const loadedProducts = plainToClass(Product, products)
+// const loadedProducts = products.map(product =>{
+//   return new Product(product.title, product.price)
+// })
 // const p1 = new Product('商品',100)
 // console.log(p1.getInformation())
 for (const prod of loadedProducts){
