bd840c2e16c6e06c0440a8f6e84f1707e6b422cd
kai815 <h.kaizuka815@gmail.com>
2020-09-28 06:47:28 +0900

    no library

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,20 @@ import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
 
 import _ from 'lodash';
+import { Product } from "./product.model"
+
+const products = [
+  {title:'商品1', price:100},
+  {title:'商品2', price:200}
+]
+const loadedProducts = products.map(product =>{
+  return new Product(product.title, product.price)
+})
+// const p1 = new Product('商品',100)
+// console.log(p1.getInformation())
+for (const prod of loadedProducts){
+  console.log(prod.getInformation())
+}
 
 console.log(_.shuffle([1,2,3]))
 declare var GLOBAL:string
