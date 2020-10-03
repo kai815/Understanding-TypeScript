d1b35451734b094c07a66d32ba002bcfcb079617
kai815 <h.kaizuka815@gmail.com>
2020-10-03 17:10:35 +0900

    display todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import TodoList from './components/TodoList'
 
 //ReatのFunction Componentの型を定義
 const App: React.FC = () => {
+  const todos = [{id: 't1', text: 'TSコースの完了'}]
   return (
     <div className="App">
-      <p>Hello React!</p>
+      <h1>Hello React!</h1>
+      <TodoList items={todos}></TodoList>
     </div>
   );
 }
