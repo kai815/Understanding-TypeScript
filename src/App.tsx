10fe5cf0bb07060861ee5ca54ff974398904010c
kai815 <h.kaizuka815@gmail.com>
2020-10-04 10:35:47 +0900

    add todoAddHandler

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import NewTodo from './components/NewTodo'
 //ReatのFunction Componentの型を定義
 const App: React.FC = () => {
   const todos = [{id: 't1', text: 'TSコースの完了'}]
+  const todoAddHandler = (text: string) =>{
+    console.log(text)
+  }
   return (
     <div className="App">
       <h1>Hello React!</h1>
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler}/>
       <TodoList items={todos}></TodoList>
     </div>
   );
