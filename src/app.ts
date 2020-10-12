8f161e5215f21cccef6e926b38169449c6cc5282
kai815 <h.kaizuka815@gmail.com>
2020-10-13 06:38:36 +0900

    install and setup dotenv-webpack

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+const API_KEY = process.env.API_KEY;
+
 function searchAddressHandler(event:Event){
   event.preventDefault();
-  const enteredAddress = addressInput.value
+  // const enteredAddress = addressInput.value
 }
 
 form.addEventListener('submit',searchAddressHandler);
